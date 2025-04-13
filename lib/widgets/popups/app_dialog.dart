80c406dfb42d85dc0bed4f6b6c54bff0823bdb85
AdiSatR7 <adisatr7@gmail.com>
2025-04-13 18:41:01 +0700

    feat: add showAppDialog function for custom dialog popup

---
+++ lib/widgets/popups/app_dialog.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:blood_donor/core/theme.dart';
+
+void showAppDialog({
+  required String title,
+  required String message,
+  String confirmText = 'Tutup',
+  VoidCallback? onConfirm,
+}) {
+  Get.dialog(
+    Dialog(
+      backgroundColor: AppColors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+      insetPadding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 24, 20, 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Text: Title
+            Text(
+              title,
+              style: AppTextStyles.subheading,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+
+            // Text: Message
+            Text(
+              message,
+              style: AppTextStyles.bodyGray,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 12),
+
+            // Button: Close
+            const Divider(height: 1, color: AppColors.secondary),
+            InkWell(
+              borderRadius: BorderRadius.circular(14),
+              onTap: () {
+                Get.back();
+                onConfirm?.call();
+              },
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 14),
+                child: Text(
+                  confirmText,
+                  style: AppTextStyles.bodyBold.copyWith(
+                    color: AppColors.primary,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
