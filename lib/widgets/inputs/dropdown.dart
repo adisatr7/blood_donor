9fba0c52627a63b43e221eb37a519c3757f687ee
AdiSatR7 <adisatr7@gmail.com>
2025-04-14 11:17:33 +0700

    feat: add Dropdown widget for customizable selection input

---
+++ lib/widgets/inputs/dropdown.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:blood_donor/core/theme.dart';
+
+class Dropdown extends StatelessWidget {
+  final String label;
+  final RxString? selectedValue;
+  final List<String> options;
+  final String? placeholder;
+
+  const Dropdown({
+    super.key,
+    required this.label,
+    required this.selectedValue,
+    required this.options,
+    this.placeholder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Label for the dropdown field
+        Text(label, style: AppTextStyles.body),
+
+        // Dropdown container
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors.secondary,
+            boxShadow: [AppStyles.cardShadow],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(12, 0, 4, 0),
+            child: Obx(
+              () => DropdownButtonFormField<String>(
+                value:
+                    selectedValue?.value.isNotEmpty == true
+                        ? selectedValue!.value
+                        : null,
+                decoration: InputDecoration(
+                  fillColor: AppColors.secondary,
+                  border: InputBorder.none,
+                  hintText: placeholder ?? 'Select an option',
+                  hintStyle: AppTextStyles.bodyGray,
+                ),
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    if (selectedValue != null) {
+                      selectedValue!.value = newValue;
+                    }
+                  }
+                },
+                items:
+                    options.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value, style: AppTextStyles.body),
+                      );
+                    }).toList(),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
