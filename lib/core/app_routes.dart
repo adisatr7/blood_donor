974e529075e21cae2df4a8d23d0d999fdcc2946d
AdiSatR7 <adisatr7@gmail.com>
2025-04-19 13:17:36 +0700

    feat: implement EditProfileView and EditProfileController for user profile editing (note: none of the functions are working yet)

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -4,7 +4,7 @@ class AppRoutes {
   static const String addressSignUp = '/address-signup';
   static const String home = '/home';
   static const String settings = '/settings';
-  static const String editProfile = '$settings/edit-profile';
-  static const String editAddress = '$settings/edit-address';
-  static const String editPassword = '$settings/edit-password';
+  static const String editProfile = '$settings/edit-profile'; // TODO: Use url param for user ID
+  static const String editAddress = '$settings/edit-address'; // TODO: Use url param for user ID
+  static const String editPassword = '$settings/edit-password'; // TODO: Use url param for user ID
 }
