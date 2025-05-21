f51ec46746cd739ea27bbd2b9230276823af721b
AdiSatR7 <adisatr7@gmail.com>
2025-05-22 01:44:24 +0700

    feat: attack real API to login controller

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,64 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:dio/dio.dart';
 
-import 'package:blood_donor/services/user_service.dart';
-import 'package:blood_donor/controllers/global_controller.dart';
-import 'package:blood_donor/utils/simulate_wait.dart';
-import 'package:blood_donor/models/user.dart';
+import 'package:blood_donor/services/auth_service.dart';
 import 'package:blood_donor/widgets/popups/app_dialog.dart';
 import 'package:blood_donor/core/app_routes.dart';
+import 'package:blood_donor/models/api/auth/login_request.dart';
+import 'package:blood_donor/models/api/auth/login_response.dart';
 
 class LoginController extends GetxController {
-  final UserService _userService = UserService.instance;
-  final GlobalController _globalController = Get.find<GlobalController>();
+  final GetStorage _storageClient = GetStorage();
+  final AuthService _authService = AuthService.instance;
 
   final TextEditingController nikController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final isLoading = false.obs;
 
-  /// Login method to authenticate the user
-  Future<void> login() async {
-    String nik = nikController.text.trim();
-    String password = passwordController.text.trim();
+  /// Boolean method untuk dipasangkan pasangkan ke prop `isDisabled` pada
+  /// tombol Login untuk mematikan tombol jika inputan NIK dan password tidak valid
+  RxBool isLoginButtonDisabled() {
+    // Cek apakah inputan NIK dan password kosong
+    bool isInputEmpty = nikController.text.isEmpty || passwordController.text.isEmpty;
 
-    // Simulate API call delay
-    isLoading.value = true;
-    await simulateWait();
+    // Cek apakah inputan NIK dan password terlalu pendek
+    // - NIK: minimal 16 karakter
+    // - Password: minimal 8 karakter
+    bool isInputTooShort = nikController.text.length < 16 && passwordController.text.length < 8;
 
-    // Fetch user from the database
-    User? user = await _userService.getByNikAndPassword(nik, password);
-    isLoading.value = false;
-
-    // If user not found, show error message
-    if (user == null) {
-      showAppDialog(
-        title: 'Gagal Masuk',
-        message: 'NIK atau kata sandi salah!',
-      );
-      return;
-    }
-
-    // Save user to global controller
-    _globalController.currentUser = user;
-
-    // Navigate to home page
-    goToHomePage();
+    // Jika ada salah satu saja yang `true`, maka tombol Login akan dimatikan
+    return (isInputEmpty || isInputTooShort).obs;
   }
 
-  /// Go to the registration page
-  void goToSignUpPage() {
+  /// Method handler untuk dipasangkan ke tombol Login
+  Future<void> handleLogin() async {
+    try {
+      // Ambil data dari inputan NIK dan password
+      String nik = nikController.text.trim();
+      String password = passwordController.text.trim();
+
+      // Mulai animasi loading
+      isLoading.value = true;
+
+      // Siapkan request payload untuk dikirim ke server
+      final LoginRequest request = LoginRequest(nik: nik, password: password);
+
+      // Kirim request ke server dan terima response data
+      final LoginResponse response = await _authService.login(request);
+
+      // Ambil JWT token dari response data dan simpan ke penyimpanan lokal
+      final String token = response.token;
+      _storageClient.write('token', token);
+
+      // Jika login berhasil, buka halaman utama (Home)
+      _goToHomePage();
+    } on DioException catch (e) {
+      // Jika login gagal, tampilkan dialog error
+      showAppError('Login Gagal', e);
+    } finally {
+      // Baik berhasil maupun gagal, hentikan animasi loading
+      isLoading.value = false;
+    }
+  }
+
+  /// Handler method untuk dipasangkan ke tombol Daftar Akun
+  void handleSignup() {
+    _goToSignupPage();
+  }
+
+  /// Method internal untuk membuka halaman Sign Up (Daftar Akun)
+  void _goToSignupPage() {
     Get.toNamed(AppRoutes.signUp);
   }
 
-  /// Go to home page
-  void goToHomePage() {
+  /// Method internal untuk membuka halaman utama (Home)
+  void _goToHomePage() {
     Get.offAllNamed(AppRoutes.home);
   }
-}
-
-class LoginResponse {
-  final bool success;
-  final String message;
-
-  LoginResponse({required this.success, required this.message});
 }
