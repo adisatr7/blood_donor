1b5fb0cad6d2a6fa3c40a49810931685c9a2f078
AdiSatR7 <adisatr7@gmail.com>
2025-05-25 12:36:57 +0700

    refactor: remove GlobalController as it's no longer needed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'package:blood_donor/controllers/global_controller.dart';
 import 'package:blood_donor/core/theme.dart';
 import 'package:blood_donor/core/app_routes.dart';
 import 'package:blood_donor/views/login_view.dart';
@@ -21,8 +20,6 @@ void main() async {
 
   // Load environment variables from .env file
   await dotenv.load(fileName: '.env');
-  // Register global controllers
-  Get.put(GlobalController(), permanent: true);
 
   // Initialize the app and routing
   runApp(
@@ -39,7 +36,7 @@ void main() async {
         GetPage(name: AppRoutes.editProfile, page: () => EditProfileView()),
         GetPage(name: AppRoutes.editAddress, page: () => EditAddressView()),
         GetPage(name: AppRoutes.editPassword, page: () => EditPasswordView()),
-        GetPage(name: AppRoutes.questionareForm,page: () => QuestionnaireFormView()),
+        GetPage(name: AppRoutes.questionareForm, page: () => QuestionnaireFormView()),
       ],
       initialRoute: AppRoutes.login,
     ),
