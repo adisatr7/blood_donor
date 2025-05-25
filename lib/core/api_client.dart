97663df41d1cee38cc79c36e5c0dd7262d20dbf6
AdiSatR7 <adisatr7@gmail.com>
2025-05-25 23:22:25 +0700

    fix: enhance error handling in ApiClient by consolidating connection and token checks

--- lib/core/api_client.dart
+++ lib/core/api_client.dart
@@ -6,22 +6,31 @@ import 'package:get/get.dart';
 import 'package:blood_donor/core/app_routes.dart';
 
 class ApiClient {
-  static final Dio instance =
-      Dio(
-          BaseOptions(
-            baseUrl: '${dotenv.env['API_URL']}/api/v1',
-            connectTimeout: const Duration(seconds: 10),
-            receiveTimeout: const Duration(seconds: 10),
-          ),
-        )
-        ..interceptors.add(
-          InterceptorsWrapper(
-            // Masukkan token ke dalam header semua request
-            onRequest: _injectToken,
-            // Cek apakah token sudah kadaluarsa setiap ada error
-            onError: _checkForExpiredToken,
-          ),
-        );
+  static final Dio instance = Dio(
+      BaseOptions(
+        baseUrl: '${dotenv.env['API_URL']}/api/v1',
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+      ),
+    )
+    ..interceptors.add(
+      InterceptorsWrapper(
+        // Masukkan token ke dalam header semua request
+        onRequest: _injectToken,
+        // Handle segala macam error
+        onError: (DioException error, handler) async {
+          // Cek apakah ada masalah koneksi
+          _checkforConnectionError(error, handler);
+
+          // Cek apakah token kadaluarsa
+          _checkForExpiredToken(error, handler);
+
+          // Jika error disebabkan karena hal lain, lanjutkan request agar
+          // error handling diurus oleh controller
+          handler.next(error);
+        },
+      ),
+    );
 
   /// Method internal untuk menyuntikkan token ke dalam header setiap request
   /// yang dikirim ke server. Beberapa API endpoint di Backend memerlukan
@@ -37,22 +46,34 @@ class ApiClient {
     handler.next(options);
   }
 
+  /// Method internal untuk mengecek apakah ada masalah koneksi
+  static void _checkforConnectionError(DioException error, handler) {
+    // Jika error karena masalah koneksi, tampilkan dialog error
+    if (error.type == DioExceptionType.connectionTimeout ||
+        error.type == DioExceptionType.sendTimeout ||
+        error.type == DioExceptionType.receiveTimeout ||
+        error.type == DioExceptionType.connectionError) {
+      // Buat pesan error untuk ditampilkan di View
+      DioException newError = DioException(
+        requestOptions: error.requestOptions,
+        type: DioExceptionType.connectionError,
+        message: 'Koneksi ke server gagal. Periksa koneksi internet Anda.',
+      );
+
+      handler.reject(newError); // Jangan lanjutkan request
+    }
+  }
+
   /// Method internal untuk mengecek apakah token sudah kadaluarsa
   /// atau tidak valid. Jika ya, hapus semua token dari penyimpanan lokal
   /// dan lempar user kembali ke halaman login
-  static void _checkForExpiredToken(DioException error, handler) async {
+  static void _checkForExpiredToken(DioException error, handler) {
     // Jika error 403, berarti token sudah kadaluarsa atau tidak valid
     if (error.response?.statusCode == 403) {
-      // Hapus semua token dari storage
-      GetStorage().erase();
-      // Lempar ke halaman login
-      Get.offAllNamed(AppRoutes.login);
-      // Jangan lanjutkan request
-      handler.reject(error);
-      return;
+      GetStorage().erase(); // Hapus semua token dari storage
+      Get.offAllNamed(AppRoutes.login); // Lempar ke halaman login
+
+      handler.reject(error); // Jangan lanjutkan request
     }
-    // Jika error disebabkan karena hal lain, lanjutkan request agar
-    // error handling diurus oleh controller
-    handler.next(error);
   }
 }
