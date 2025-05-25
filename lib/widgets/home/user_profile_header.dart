bd07cc520849ff08192e72af40c7f5fa0bf5fd62
AdiSatR7 <adisatr7@gmail.com>
2025-05-25 22:38:28 +0700

    refactor: change gear icon into hamburger menu

--- lib/widgets/home/user_profile_header.dart
+++ lib/widgets/home/user_profile_header.dart
@@ -6,73 +6,64 @@ import 'package:blood_donor/core/theme.dart';
 
 class UserProfileHeader extends StatelessWidget {
   final Rxn<User> currentUser;
-  final VoidCallback onGearIconPress;
+  final VoidCallback onIconPress;
 
   const UserProfileHeader({
     super.key,
     required this.currentUser,
-    required this.onGearIconPress,
+    required this.onIconPress,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: Obx(
-        () => Row(
-          children: <Widget>[
-            // 📸 Foto profil
-            CircleAvatar(
-              radius: 28,
-              backgroundColor: Colors.grey[300],
-              backgroundImage: NetworkImage(
-                currentUser.value?.profilePicture?.path ?? '',
+    return Obx(
+      () => Row(
+        children: <Widget>[
+          // 📸 Foto profil
+          CircleAvatar(
+            radius: 28,
+            backgroundColor: Colors.grey[300],
+            backgroundImage: NetworkImage(
+              currentUser.value?.profilePicture?.path ?? '',
+            ),
+          ),
+
+          // Text group berisi nama user dan golongan darah
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // 🧑‍⚕️ Nama user
+                  Text(
+                    currentUser.value?.name ?? '',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  // 🩸 Golongan Darah
+                  RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        const TextSpan(
+                          text: 'Golongan Darah: ',
+                          style: AppTextStyles.bodyGray,
+                        ),
+                        TextSpan(
+                          text: currentUser.value?.mergedBloodType ?? '-',
+                          style: AppTextStyles.bodyGrayBold,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
+          ),
 
-            // Text group berisi nama user dan golongan darah
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    // 🧑‍⚕️ Nama user
-                    Text(
-                      currentUser.value?.name ?? '',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    // 🩸 Golongan Darah
-                    RichText(
-                      text: TextSpan(
-                        children: <TextSpan>[
-                          const TextSpan(
-                            text: 'Golongan Darah: ',
-                            style: AppTextStyles.bodyGray,
-                          ),
-                          TextSpan(
-                            text: currentUser.value?.mergedBloodType ?? '-',
-                            style: AppTextStyles.bodyGrayBold,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            // ⚙️ Tombol Pengaturan
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: onGearIconPress,
-            ),
-          ],
-        ),
+          // ⚙️ Tombol Pengaturan
+          IconButton(icon: const Icon(Icons.menu), onPressed: onIconPress),
+        ],
       ),
     );
   }
