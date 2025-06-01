f8afe5b07b04a71e720578520a135ed92d5f633d
AdiSatR7 <adisatr7@gmail.com>
2025-06-01 11:03:15 +0700

    feat: implement LocationService for fetching and searching locations

---
+++ lib/services/location_service.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+
+import 'package:blood_donor/core/api_client.dart';
+import 'package:blood_donor/models/db/location.dart';
+
+class LocationService {
+  static final LocationService instance = LocationService();
+  final Dio _apiClient = ApiClient.instance;
+
+  /// Mengambil semua lokasi dari server
+  Future<List<Location>> getAllLocations() async {
+    final response = await _apiClient.get('/locations');
+
+    if (response.data == null || response.data['success'] == false) {
+      throw Exception('Gagal mengambil lokasi: ${response.statusCode}');
+    }
+
+    return (response.data['data'] as List)
+        .map((loc) => Location.fromMap(loc))
+        .toList();
+  }
+
+  /// Mencari nama lokasi berdasarkan query
+  Future<List<Location>> searchLocations(String query) async {
+    // Jika query kosong, serahkan hasil kosong. Pastikan di
+    // MapController jika search query kosong, panggil method
+    // getAllLocations() saja
+    if (query.isEmpty) {
+      return [];
+    }
+
+    final response = await _apiClient.get('/locations/search',
+      queryParameters: {'name': query},
+    );
+
+    if (response.data == null || response.data['success'] == false) {
+      throw Exception('Gagal mencari lokasi: ${response.statusCode}');
+    }
+
+    return (response.data['data'] as List)
+        .map((loc) => Location.fromMap(loc))
+        .toList();
+  }
+}
