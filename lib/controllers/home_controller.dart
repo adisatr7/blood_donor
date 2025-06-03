5428325be8771513ab3cf2a187f0819369d2cea2
AdiSatR7 <adisatr7@gmail.com>
2025-06-03 23:52:06 +0700

    fix: update appointment fetching method to use assignAll for reactive updates

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,4 +1,3 @@
-import 'package:blood_donor/models/db/user.dart';
 import 'package:get/get.dart';
 
 import 'package:blood_donor/services/appointment_service.dart';
@@ -6,6 +5,7 @@ import 'package:blood_donor/controllers/global_controller.dart';
 import 'package:blood_donor/models/db/appointment.dart';
 import 'package:blood_donor/core/app_routes.dart';
 import 'package:blood_donor/widgets/popups/app_dialog.dart';
+import 'package:blood_donor/models/db/user.dart';
 
 class HomeController extends GetxController {
   final AppointmentService _appointmentService = AppointmentService.instance;
@@ -48,7 +48,7 @@ class HomeController extends GetxController {
   }
 
   Future<void> _fetchAppointments() async {
-    appointments.value = await _appointmentService.getAll();
+    appointments.assignAll(await _appointmentService.getAll());
   }
 
   /// Method internal untuk mengecek apakah user sudah mengisi alamat
